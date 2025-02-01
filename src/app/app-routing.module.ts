ee4167fb51f639ad8b5a23d9477703ec7995d087
Jordan Fisher <jfisher3.writer@gmail.com>
2025-02-01 15:52:58 -0500

    Added routes and navbar for Homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AngerComponent } from './anger/anger.component';
+import { JoyComponent } from './joy/joy.component';
+import { AnxietyComponent } from './anxiety/anxiety.component';
+import { HopeComponent } from './hope/hope.component';
+import { LoveComponent } from './love/love.component';
+import { PeaceComponent } from './peace/peace.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'anger', component: AngerComponent },
+  { path: 'joy', component: JoyComponent },
+  { path: 'anxiety', component: AnxietyComponent },
+  { path: 'hope', component: HopeComponent },
+  { path: 'love', component: LoveComponent },
+  { path: 'peace', component: PeaceComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
