5b2846d54216d0917e978cfb96ecbfd6d0f714e3
Jordan Fisher <jfisher3.writer@gmail.com>
2025-02-18 08:22:44 -0500

    Added styling to Register and Login pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   email: string = '';
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   email: string = '';
